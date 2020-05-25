e5c9b2981df4b8a01fe712feb64fbccbbf8819d1
nacho <nichod@wow.com>
2020-05-25 20:35:54 +0200

    Fixed some errors in SULDR, added print user and enabled remote access

--- drivers/suldr.sh
+++ drivers/suldr.sh
@@ -2,22 +2,44 @@
 # This script install the Samsung unified linux drivers
 #
 # Supported variables
-# `SULDR_DRIVER_VERSION` (Default: 4.00.39) - Version of suldr-driver to install
 # `SULDR_DRIVER2_VERSION` (Default: 1.00.39) - Version of suldr-driver2 to install
 
 source ${PREFIX}/functions
 
-SULDR_DRIVER_VERSION="${FOOMATIC_FILTERS_VERSION:-1.00.39}"
-SULDR_DRIVER2_VERSION="${FOOMATIC_FILTERS_VERSION:-1.00.39}"
+SULDR_DRIVER2_VERSION="${SULDR_DRIVER2_VERSION:-1.00.39}"
 
-if (isInst suld-driver2-${SULDR_DRIVER2_VERSION} || isInst suld-driver-${SULDR_DRIVER_VERSION}); then
+if (isInst suld-driver2-${SULDR_DRIVER2_VERSION}); then
     echo "suldr is already installed"
     exit
 fi
 
 PKGS="\
-    suld-driver-${SULDR_DRIVER1_VERSION}\
-	suld-driver2-${SULDR_DRIVER2_VERSION}"
+    suld-driver2-${SULDR_DRIVER2_VERSION}\
+	suld-ppd-4\
+	suld-driver2-common-1"
+
+DEPS="\
+    curl\
+	dirmngr\
+    gnupg\
+    gnupg-l10n\
+    gnupg-utils\
+    gpg\
+    gpg-agent\
+    gpg-wks-client\
+    gpg-wks-server\
+    gpgconf\
+    gpgsm\
+    libassuan0\
+    libksba8\
+    libnpth0\
+    libreadline7\
+    lsb-base\
+    pinentry-curses\
+    readline-common"
+
+apt-get update
+apt-get -qy install ${DEPS}
 
 echo "deb https://www.bchemnet.com/suldr/ debian extra" > /etc/apt/sources.list.d/suldr.list
 curl -L -o suldr-keyring.deb http://www.bchemnet.com/suldr/pool/debian/extra/su/suldr-keyring_2_all.deb
